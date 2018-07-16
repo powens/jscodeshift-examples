1dd599ee15614304af7b30a4900cca63669d1a2a
Patrick Owens <1919554+powens@users.noreply.github.com>
2018-07-15 23:47:13 -0700

    Add test-utils-with-find

---
+++ src/__testfixtures__/rename-test-utils-with-find.input.js
@@ -0,0 +1,6 @@
+import TestUtils from 'react-addons-test-utils';
+import Widget from './Widget';
+
+function testWidget() {
+  // doing some useful stuff here
+}
--- src/remove-debugger.js
+++ src/remove-debugger.js
@@ -1,11 +1,12 @@
 /**
+ * remove-debugger.js
  * Removes all debugger statements
  */
-module.exports = function(file, api) {
+module.exports = function(file, api, printOptions) {
   const j = api.jscodeshift;
 
   return j(file.source)
     .find(j.DebuggerStatement)
     .remove()
-    .toSource();
+    .toSource(printOptions);
 };
